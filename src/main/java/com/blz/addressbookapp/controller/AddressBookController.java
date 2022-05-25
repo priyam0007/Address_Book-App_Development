98602aaa72aba180dcbdf1b389e0fd2bc5cb93e0
Your Name <you@example.com>
2022-05-26 00:26:40 +0530

    Added: created DTO and Model layers

--- src/main/java/com/blz/addressbookapp/controller/AddressBookController.java
+++ src/main/java/com/blz/addressbookapp/controller/AddressBookController.java
@@ -1,6 +1,8 @@
 package com.blz.addressbookapp.controller;
 
 import com.blz.addressbookapp.dto.AddressBookDTO;
+import com.blz.addressbookapp.dto.ResponseDTO;
+import com.blz.addressbookapp.model.AddressBookData;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,27 +12,37 @@ import org.springframework.web.bind.annotation.*;
 public class AddressBookController {
 
     @RequestMapping("/get")
-    public ResponseEntity<String> getAddressBookData() {
-        return new ResponseEntity<String>("Get call Success", HttpStatus.OK);
+    public ResponseEntity<ResponseDTO> getAddressBookData() {
+        AddressBookData addressBookData=new AddressBookData(1,new AddressBookDTO("Priya","Madiwalar",
+                "9876543218","developer@gmail.com","bijapur","karnatak",586109));
+        ResponseDTO responseDTO=new ResponseDTO("Get call Success",addressBookData);
+        return  new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.OK);
     }
-
     @GetMapping("/{id}")
-    public ResponseEntity<String> getAddressBookData(@PathVariable int id){
-        return  new ResponseEntity<String>("Get call Success for id"+id, HttpStatus.OK);
+    public ResponseEntity<ResponseDTO> getAddressBookData(@PathVariable int id){
+        AddressBookData addressBookData=new AddressBookData(1,new AddressBookDTO("Priya","Madiwalar",
+                "9876543218","developer@gmail.com","bijapur","karnatak",586109));
+        ResponseDTO responseDTO=new ResponseDTO("Get call for ID Success",addressBookData);
+        return  new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.OK);
     }
     @PostMapping("/create")
-    public ResponseEntity<String> createAddressBookData(@RequestBody AddressBookDTO addressBookDTO){
-        return  new ResponseEntity<String>("created Address Book data for"+addressBookDTO, HttpStatus.OK);
+    public ResponseEntity<ResponseDTO> createAddressBookData(@RequestBody AddressBookDTO addressBookDTO){
+        AddressBookData addressBookData = new AddressBookData(1,addressBookDTO);
+        ResponseDTO responseDTO=new ResponseDTO("created Address Book succesfully",addressBookData);
+        return  new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.OK);
     }
 
     @PutMapping("/update")
-    public ResponseEntity<String> updateAddressBookData(@RequestBody AddressBookDTO addressBookDTO) {
-        return  new ResponseEntity<String>("updated Address Book data for"+addressBookDTO, HttpStatus.OK);
+    public ResponseEntity<ResponseDTO> updateAddressBookData(@RequestBody AddressBookDTO addressBookDTO) {
+        AddressBookData addressBookData = new AddressBookData(1,addressBookDTO);
+        ResponseDTO responseDTO=new ResponseDTO("update Address Boook succesfully",addressBookData);
+        return  new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteAddressBookData(@PathVariable int id){
-        return  new ResponseEntity<String>("delete call Success for id"+id, HttpStatus.OK);
+    public ResponseEntity<ResponseDTO> deleteAddressBookData(@PathVariable int id){
+        ResponseDTO responseDTO=new ResponseDTO("deleted succesfully",id);
+        return  new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.OK);
     }
 
 }
---
+++ src/main/java/com/blz/addressbookapp/model/AddressBookData.java
@@ -0,0 +1,110 @@
+package com.blz.addressbookapp.model;
+
+import com.blz.addressbookapp.dto.AddressBookDTO;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class AddressBookData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private int id;
+
+    private String fName;
+
+    private String lName;
+
+    private String phonenumber;
+
+    private String email;
+
+    private String city;
+
+    private String state;
+
+    private long zipcode;
+
+    public AddressBookData() {
+
+    }
+
+    public int getid() {
+        return id;
+    }
+
+    public void setid(int id) {
+        this.id = id;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public long getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(long zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public AddressBookData(int id, AddressBookDTO addressBookDTO) {
+        this.id = id;
+        this.fName = addressBookDTO.getfName();
+        this.lName = addressBookDTO.getlName();
+        this.phonenumber = addressBookDTO.getPhonenumber();
+        this.email = addressBookDTO.getEmail();
+        this.city = addressBookDTO.getCity();
+        this.state = addressBookDTO.getState();
+        this.zipcode = addressBookDTO.getZipcode();
+    }
+}
